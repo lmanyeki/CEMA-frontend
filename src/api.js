881ade8878bd76fd9e07c44a2e6af89506948936
lmanyeki-debug <lydiahmanyeki@gmail.com>
2025-04-26 17:58:48 +0300

    deployed links

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  // baseURL: 'https://cema-backend.onrender.com',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
 });
 
 export const createProgram = (programData) => api.post('/programs', programData);
